0d8b3f8b8eb7750c29252c52f0cbaafe3141a197
ARDA <ardagenc29@hotmail.com>
2026-05-06 23:28:48 +0300

    DCAU izleme rehberi ilk versiyonu yazıldı

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Merhaba Dunya") 
+# DCAU İzleme Rehberi - Versiyon 1.0
+
+print("--- DCAU Rehberine Hoş Geldin! ---")
+
+# İzlediklerimizi bir listede tutuyoruz.
+
+izlenen_diziler = ["Batman: The Animated Series", "The New Batman Adventures"]
+
+# Sıradaki diziyi bir değişkene atıyoruz.
+
+siradaki_dizi = "Batman Beyond"
+
+print("Şu ana kadar bitirdiklerin:", izlenen_diziler)
+print("Sana tavsiyem, artık şu seriye başlamalısın:", siradaki_dizi)
